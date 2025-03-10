cf7f02b9e6d161e6bfb02abf50aaa920b0097e10
techzain <mdalimurtaza78689@gmail.com>
2025-03-10 22:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", ":")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "28225971"))
+API_ID = int(os.environ.get("API_ID", ""))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "d5de247a7d8b865b48d1bb5944058315
+API_HASH = os.environ.get("API_HASH", "
 
 1 17:16")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "5943224894"))
+ADMINS = int(os.environ.get("ADMINS", ""))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://mdalimurtaza78689:2MIzJuEaFpMvsJCK@cluster0.1ysrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", .") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
